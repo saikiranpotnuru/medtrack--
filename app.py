e8c33d4815b9c49a51e0955fb6c2f22a2b1215f5
saikiranpotnuru <115359052+saikiranpotnuru@users.noreply.github.com>
2025-07-05 10:26:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -889,22 +889,14 @@ def add_patient():
     else:
         flash('DynamoDB is required for production. Please enable it.', 'danger')
         return redirect(url_for('doctor_dashboard'))
-        app.run(host='0.0.0.0', port=5000, debug=True)
-
+        
 # dynamodb = False   
-
-
 
 # Route: Select role
 @app.route('/')
 def index():
     return render_template('index.html')
-    
-
-
-
-
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
